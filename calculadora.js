37dd2ece3c638724a4038533fb13f15eccaf5b2d
clarinhaifc <Claraeu15@outlook.com>
2022-04-01 09:12:17 -0300

    1.1.2

--- calculadora.js
+++ calculadora.js
@@ -1,3 +1,15 @@
 var somar = require("./modulos/somar");
 
 console.log(somar(20,90));
+
+var subtrair = require ("./modulos/subtrair");
+
+console.log(subtrair(90,20));
+
+var multiplicar = require ("./modulos/multiplicar"); 
+
+console.log(multiplicar(20,20));
+
+var dividir = require ("./modulos/dividir");
+
+console.log(dividir(90,30));
